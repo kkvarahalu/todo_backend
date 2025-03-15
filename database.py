3df21279f82ca4b0d28bfd4d12b540ed46b3dfae
kkvarahalu <kalimikanakavarahalu1@gmail.com>
2025-03-15 21:12:58 +0530

    external db integrated

--- database.py
+++ database.py
@@ -1,12 +1,15 @@
+import os
+import psycopg2
 import mysql.connector
 
 def get_db_connection():
-    conn=mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="pallavi",
-        database="todo_db"
-    )
+    conn = psycopg2.connect(os.getenv('postgresql://kanakam:zwkjzdx67SgAxK8HV1yelfCDUTuzqWRR@dpg-cvapsbqj1k6c738vtn8g-a/todo_db_js7l'))
+    # conn=mysql.connector.connect(
+    #     host="localhost",
+    #     user="root",
+    #     password="pallavi",
+    #     database="todo_db"
+    # )
     return conn
 
 def init_db():
